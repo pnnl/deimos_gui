75bfeca57ce3b7f3af0436f6d71b89e217cedcde
Oostrom, Marjolein T <marjolein.oostrom@pnnl.gov>
2024-02-06 15:01:32 -0800

    add tests

---
+++ src/tests/test_app_frontend.py
@@ -0,0 +1,19 @@
+import time
+
+import panel as pn
+from run_app import app1
+from playwright.sync_api import expect
+
+CLICKS = 2
+
+def test_component(page, port):
+
+    # Given
+    url = f"http://localhost:{port}"
+    # When
+    server = pn.serve(app1, port=port, threaded=True, show=False)
+    time.sleep(0.2)
+    page.goto(url, timeout = 0)   
+    server.stop()
+
+
